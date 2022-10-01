6adcf33319879d1e33c17f0813ba192948545863
chafaa <chafaaouchaou@gmail.com>
2022-10-01 22:26:51 +0100

    get rid of config.py

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ bottona.click()
 
 
 
+
+
 # # qi72231t n3hqoq4p r86q59rh b3qcqh3k fq87ekyn fsf7x5fv s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk cr00lzj9 rn8ck1ys s3jn8y49 f14ij5to l3ldwz01 icdlwmnq i85zmo3j qmqpeqxj e7u6y3za qwcclf47 nmlomj2f frfouenu bonavkto djs4p424 r7bn319e bdao358l alzwoclg jcxyg2ei srn514ro oxkhqvkx rl78xhln nch0832m om3e55n1 nq2b4knc bis24fgy a5wdgl2o
 
 
